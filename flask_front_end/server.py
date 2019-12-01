de55a7a9cd1463f55c176a2af36a616303466a15
Katerina Chinnappan <kachinna@ucsc.edu>
2019-12-01 20:25:26 +0100

    trying to make a POST request

--- flask_front_end/server.py
+++ flask_front_end/server.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
+import sys
 app = Flask(__name__)
 
-@app.route('/index')
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-   array = ['TOEFL-IBT', 'IELTS', 'Cambridge', 'VU Test']
+   if request.method=='GET':
+       print('hiiii', file=sys.stdout)
+       array = ['TOEFL-IBT', 'IELTS', 'Cambridge', 'VU Test']
+   elif request.method=='POST':
+       print('hi', file=sys.stdout)
+       text = request.form.get('etestt')
+       processed_text = text.upper()
+       print(processed_text, file=sys.stdout)
    return render_template('index.html', index = array)
+
+@app.route('/index', methods=['POST'])
+def index_post():
+    print('hi', file=sys.stdout)
+    text = request.form.get('etestt')
+    processed_text = text.upper()
+    print(processed_text, file=sys.stdout)
+    return processed_text
 
 if __name__ == '__main__':
    app.run(debug = True)
