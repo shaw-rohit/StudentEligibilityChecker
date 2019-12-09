bf9c017ee1b337d6a17648ceb3b26f3385056895
Katerina Chinnappan <kachinna@ucsc.edu>
2019-12-09 11:08:01 +0100

    trying ontology stuff

--- flask_front_end/server.py
+++ flask_front_end/server.py
@@ -1,9 +1,26 @@
 from flask import Flask, request, render_template
+import rdflib
 import sys
 app = Flask(__name__)
 
+ontology = "/Users/agukalpa/Documents/VU Amsterdam/Knowledge Engineering/StudentEligibilityChecker/flask_front_end/Ontology.ttl" 
+graph = rdflib.Graph()
+graph.parse(ontology, format='ttl')
+#print(result)
+qres = graph.query(
+    """SELECT DISTINCT ?Track ?ExtraKnowledge
+       WHERE {
+          ?a sec:Track ?Track .
+          ?b sec:ExtraKnowledge ?ExtraKnowledge
+       }""")
+
+#print("query:", qres)
+for row in qres:
+    print("%s requiresKnowledge %s" % row)
+
 @app.route('/index', methods=['GET', 'POST'])
 def index():
+   print('sup')
    if request.method=='GET':
        print('hiiii', file=sys.stdout)
        array = ['TOEFL-IBT', 'IELTS', 'Cambridge', 'VU Test']
